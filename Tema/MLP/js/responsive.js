ae5ba57046a558ef4e24e5677b6a928a394f80c4
Henrik Petersson <hp222bf@student.lnu.se>
2013-03-05 15:57:16 +0100

    Fixat bugg som gjorde att dropdown-menyn inte ändrades till aktuell sida.

--- Tema/MLP/js/responsive.js
+++ Tema/MLP/js/responsive.js
@@ -1,3 +1,9 @@
-jq("#navigation select").on('change', function() {
+$("#navigation select").on('change', function() {
 	window.location = jq(this).find("option:selected").val();
+});
+
+$("#navigation option").each(function () {
+    if ($(this).val() == window.location) {
+        $(this).prop('selected', true);
+    }
 });
